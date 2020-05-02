6c8a38c718df0c0d5be156e3bb2898a0af96ae1c
Elisey Korochkin <eliseysergeevich@yandex.ru>
2020-05-03 02:14:39 +0300

    чтение с порта...

--- Target CNC GC 08_04_20/MainWindow.xaml.cs
+++ Target CNC GC 08_04_20/MainWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Windows.Threading;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +15,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Windows.Threading;
+
+
 
 namespace Target_CNC_GC_08_04_20
 {
@@ -23,7 +27,7 @@ namespace Target_CNC_GC_08_04_20
     /// </summary>
     /// 
     
-     class WindowsViews
+    class WindowsViews
     {
         public enum WindowsView
         {
@@ -42,31 +46,85 @@ namespace Target_CNC_GC_08_04_20
     public partial class MainWindow : Window
     {
         WindowsViews temp = new WindowsViews();
-        
+        SerialPort ArduinoPort = new SerialPort();
+        private delegate void updateDelegate(string txt);
+        System.Timers.Timer aTimer;
         int count;
         public MainWindow()
         {
             InitializeComponent();
         
             temp.WindowsName = WindowsViews.WindowsView.situation;
-             
-        DispatcherTimer timer = new DispatcherTimer();
-            //timer.Tick += new EventHandler(timer_Tick);
-            //timer.Interval = new TimeSpan(0, 0, 0,0,10);
-            //timer.Start();
+            string[] ports = SerialPort.GetPortNames();
+            portsCB.ItemsSource = ports;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Tick += new EventHandler(timerCOM_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 20);
+            timer.Start();
+
+            //aTimer = new System.Timers.Timer(1500);
+            //aTimer.Elapsed += timerCOM_Tick;
+            //aTimer.AutoReset = true;
+            //aTimer.Enabled = true;
+
+            //DispatcherTimer timerCOM = new DispatcherTimer();
+            //timerCOM.Tick += new EventHandler(timerCOM_Tick);
+            //timerCOM.Interval = new TimeSpan(0, 0, 0, 0, 20);
+            //timerCOM.Start();
+
+            //ArduinoPort.PortName = portsCB.Text;
+            //ArduinoPort.BaudRate = 9600;
+            //ArduinoPort.ReceivedBytesThreshold = 40;
+            ////ArduinoPort.RtsEnable = true;
+            ////ArduinoPort.ReadTimeout = 1000;
+            //ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            //try
+            //{
+            //    ArduinoPort.Open();
+
+
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Неудачно");
+            //}
         }
 
-        //private void timer_Tick(object sender, EventArgs e)
-        //{
-        //    count++;
-        //    TimeNow.Value = count;
-        //    if (count == 100) count = 0;
-        //}
-        //public void AddCount()
-        //{
-        //    count++;
-        //    TimeNow.Value = count;
-        //}
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            count++;
+            TimeNow.Value = count;
+            if (count == 100) count = 0;
+        }
+        public void AddCount()
+        {
+            count++;
+            TimeNow.Value = count;
+        }
+
+        private void timerCOM_Tick(object sender, EventArgs e)
+        {
+            //if (ArduinoPort.IsOpen)
+            //{
+            //    try
+            //    {
+            //        // ArduinoPort.DiscardInBuffer();
+            //        string returnMessage = ArduinoPort.ReadLine();
+            //        dataSerialTB.Dispatcher.BeginInvoke(new updateDelegate(updateTextBox), returnMessage);
+
+            //        //ArduinoPort.Close();
+            //    }
+            //    catch { ArduinoPort.Close(); }
+            //}
+        }
+
+        public  void updateTextBox(string txt)
+        {
+            dataSerialTB.Text = txt;
+        }
+
 
 
         private void situation_Click(object sender, RoutedEventArgs e)
@@ -125,11 +183,54 @@ namespace Target_CNC_GC_08_04_20
            // PeopleWin.Visibility = Visibility.Visible;
         }
 
-       
+        private void conectBT_Click(object sender, RoutedEventArgs e)
+        {
+            ArduinoPort.PortName = portsCB.Text;
+            ArduinoPort.BaudRate = 9600;
+           // ArduinoPort.ReceivedBytesThreshold = 16;
+            //ArduinoPort.RtsEnable = true;
+            //ArduinoPort.ReadTimeout = 1000;
+            ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            try
+            {
+                ArduinoPort.Open();
+                
+                
+            }
+            catch
+            {
+                MessageBox.Show("Неудачно");
+            }
+        }
 
-     
+        private  void DataReceivedHandler(
+                       object sender,
+                       SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            dataSerialTB.Dispatcher.Invoke(new updateDelegate(updateTextBox), indata);
 
-      
+        }
+
+        private void disConectBT_Click(object sender, RoutedEventArgs e)
+        {
+            ArduinoPort.Close();
+        }
+
+        private void portsCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+
+
+        private void portsCB_MouseMove(object sender, MouseEventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            portsCB.ItemsSource = ports;
+        }
+        
     }
     
 
